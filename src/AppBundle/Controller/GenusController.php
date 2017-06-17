f91501c2db42543d4dc89d532fb829be1d8f0062
Code-Architect <codearchitectdeveloper@gmail.com>
2017-06-17 21:32:05 +0530

    starting symfony

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $templeting = $this->container->get('templating');
+        return new Response('Under the sea we have: '.$genusName);
+    }
+}
