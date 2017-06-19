5128ec14747c7861a8b1a8cf180c9c04b57ee675
Code-Architect <codearchitectdeveloper@gmail.com>
2017-06-19 15:36:43 +0530

    Generating URLs

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -1,11 +1,12 @@
 <?php
 
-
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -15,7 +16,38 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templeting = $this->container->get('templating');
-        return new Response('Under the sea we have: '.$genusName);
+//        $templeting = $this->container->get('templating');
+//        $html = $templeting->render('genus/show.html.twig', [
+//            'name'  => $genusName,
+//        ]);
+//        return new Response($html);
+        $notes = [
+            "hello"
+        ];
+
+        return $this->render('genus/show.html.twig', [
+            'name'  => $genusName,
+            'notes' => $notes,
+        ]);
+    }
+
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' =>  $notes,
+        ];
+
+        return new JsonResponse($data);
     }
 }
